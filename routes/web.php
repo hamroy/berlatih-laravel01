862f1e0fde08f9451219a7bf4cc6e2bbb9312630
ilhamroy <hamroy1228@gmail.com>
2020-08-05 22:42:26 +0700

    tugas templating blade selesai

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,23 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::get('/', 'HomeController@index');
+Route::get('/', 'HomeController@index'); //tugas Hari 2 – Membuat Web Statis dengan Laravel
 Route::get('/register', 'AuthController@register');
 Route::post('/welcome', 'AuthController@welcome');
+
+// lanjutan 
+Route::get('/master', function ()
+{
+	return view('adminlte/master');
+});
+
+// 
+Route::get('/', function () //Hari 3 – Memasangkan Template dengan Laravel Blade
+{
+	return view('adminlte.table.table');
+});
+
+Route::get('/data-tables', function ()
+{
+	return view('adminlte.table.data-tables');
+});
